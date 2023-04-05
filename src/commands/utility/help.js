c7be22547062b11b95b742318b013563a49f7682
April <95392008+apriltaoyvr@users.noreply.github.com>
2023-04-05 16:02:33 -0700

    Message formatting

--- src/commands/utility/help.js
+++ src/commands/utility/help.js
@@ -7,18 +7,15 @@ module.exports = {
     .setDescription('Display commands and information.'),
   async execute(interaction) {
     await interaction.reply(
-      `Hello, I am a bot to help you make decision and post cat and dog photos.
-       ${codeBlock('/help')} for info (What you are using right now!)
-
-       For making decisions:
-       ${codeBlock('/decision')} for a yes or no question 
-       ${codeBlock('/8ball')} for a magic 8 ball
-       ${codeBlock('/roll')} to roll a die, 1d20 by default
-
-       For fun:
-       ${codeBlock('/cat')} for a random cat photo
-       ${codeBlock('/dog')} for a random dog photo
-      `
+      'Hello, I am a bot to help you make decision and post cat and dog photos.\n' +
+        `${codeBlock('/help')} for info (What you are using right now!)\n` +
+        '\nFor making decisions:\n' +
+        `${codeBlock('/decision')} for a yes or no question\n` +
+        `${codeBlock('/8ball')}for a magic 8 ball\n` +
+        `${codeBlock('/roll')} to roll a die, 1d20 by default\n` +
+        '\nFor fun:\n' +
+        `${codeBlock('/cat')} for a random cat photo\n` +
+        `${codeBlock('/dog')} for a random dog photo\n`
     );
   },
 };
