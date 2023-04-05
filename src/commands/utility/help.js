6c5f7dce0a8052aeb22b5185a08a0ddcac978999
April <95392008+apriltaoyvr@users.noreply.github.com>
2023-04-05 16:06:39 -0700

    Change to inlineCode

--- src/commands/utility/help.js
+++ src/commands/utility/help.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder } = require('discord.js');
-const { codeBlock } = require('discord.js');
+const { inlineCode } = require('discord.js');
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -8,14 +8,14 @@ module.exports = {
   async execute(interaction) {
     await interaction.reply(
       'Hello, I am a bot to help you make decision and post cat and dog photos.\n' +
-        `${codeBlock('/help')} for info (What you are using right now!)\n` +
+        `${inlineCode('/help')} for info (What you are using right now!)\n` +
         '\nFor making decisions:\n' +
-        `${codeBlock('/decision')} for a yes or no question\n` +
-        `${codeBlock('/8ball')}for a magic 8 ball\n` +
-        `${codeBlock('/roll')} to roll a die, 1d20 by default\n` +
+        `${inlineCode('/decision')} for a yes or no question\n` +
+        `${inlineCode('/8ball')}for a magic 8 ball\n` +
+        `${inlineCode('/roll')} to roll a die, 1d20 by default\n` +
         '\nFor fun:\n' +
-        `${codeBlock('/cat')} for a random cat photo\n` +
-        `${codeBlock('/dog')} for a random dog photo\n`
+        `${inlineCode('/cat')} for a random cat photo\n` +
+        `${inlineCode('/dog')} for a random dog photo\n`
     );
   },
 };
